2ef591361edbd5c48dcb98e2a71968d3bc512472
RaglandDev <alexragland2003@gmail.com>
2025-04-10 10:11:03 -0700

    route example

---
+++ fisco/app/route-example/page.tsx
@@ -0,0 +1,7 @@
+function RouteExamplePage() {
+	return (
+		<div>This is an example of a page established on a new route. (localhost:3000/route-example) </div>
+	)
+}
+
+export default RouteExamplePage
