0e257892068d39a76cb26f10f5a4967051046088
RaglandDev <alexragland2003@gmail.com>
2025-04-15 11:46:12 -0700

    this should fail tests and not deploy to vercel

--- fisco/app/components/client/Home.client.tsx
+++ fisco/app/components/client/Home.client.tsx
@@ -1,16 +1,1 @@
-"use client";
-import { TestDataType } from "@/types/Home.client"
-
-type Props = {
-  testData: TestDataType[];
-};
-
-export default function ClientHome({ testData }: Props) {
-  return (
-    <ul>
-      {testData.map((item) => (
-        <li key={item.id}>{item.id}</li>
-      ))}
-    </ul>
-  );
-}
+d
