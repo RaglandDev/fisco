91ef1a75c82cf4837115e65f6701abd62808275c
root <root@DESKTOP-4VJR434.>
2025-04-21 10:28:40 -0700

    Add Clerk prebuilt auth UI to Home.client.tsx

--- fisco/app/components/client/Home.client.tsx
+++ fisco/app/components/client/Home.client.tsx
@@ -1,16 +1,32 @@
-"use client";
-import { TestDataType } from "@/types/Home.client"
+'use client';
 
-type Props = {
-  testData: TestDataType[];
-};
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
-export default function ClientHome({ testData }: Props) {
+export default function ClientHome({ testData }: { testData: any }) {
   return (
-    <ul>
-      {testData.map((item) => (
-        <li key={item.id}>{item.id}</li>
-      ))}
-    </ul>
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to Fisco!</h1>
+
+      <SignedOut>
+        <div style={{ marginBottom: '1rem' }}>
+          <SignInButton mode="modal">
+            <button style={{ marginRight: '1rem' }}>Sign In</button>
+          </SignInButton>
+
+          <SignUpButton mode="modal">
+            <button>Sign Up</button>
+          </SignUpButton>
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <div style={{ marginBottom: '1rem' }}>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </SignedIn>
+
+      {/* home page content */}
+      <pre>{JSON.stringify(testData, null, 2)}</pre>
+    </div>
   );
 }
