49af6050228f30fe82bf33e225011ab936e134c5
root <root@DESKTOP-4VJR434.>
2025-04-21 10:39:20 -0700

    added some test for the sign in/up

--- fisco/__tests__/client/Home.client.test.tsx
+++ fisco/__tests__/client/Home.client.test.tsx
@@ -1,6 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import ClientHome from "@/components/client/Home.client";
 
+jest.mock('@clerk/nextjs', () => ({
+  SignedIn: ({ children }) => <>{children}</>,
+  SignedOut: ({ children }) => <>{children}</>,
+  SignInButton: ({ children }) => <button>{children || "Sign In"}</button>,
+  SignUpButton: ({ children }) => <button>{children || "Sign Up"}</button>,
+  UserButton: () => <div>User Avatar</div>,
+  ClerkProvider: ({ children }) => <>{children}</>,
+}));
+
 describe("Home component", () => {
   it("renders list of test data", () => {
     const testData = [
