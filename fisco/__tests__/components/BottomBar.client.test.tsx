602ee57e2a647a35e2cafd5d38e618ac3a3c7b01
RaglandDev <alexragland2003@gmail.com>
2025-05-07 11:00:24 -0700

    bottom bar test

---
+++ fisco/__tests__/components/BottomBar.client.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import { vi, it, expect } from 'vitest';
+import { ClerkProvider } from '@clerk/nextjs';
+import BottomBar from '@/components/BottomBar.client'
+
+// Mock the useRouter and usePathname hooks from Next.js
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn().mockReturnValue({
+    push: vi.fn(),       // Mock push method
+    replace: vi.fn(),
+    prefetch: vi.fn(),
+  }),
+  usePathname: vi.fn().mockReturnValue('/'),  // Mock usePathname with a dummy return value
+}));
+
+
+it('renders a list of posts', async () => {
+ 
+  render(
+    <ClerkProvider>
+      <BottomBar/>
+    </ClerkProvider>
+    )
+});
